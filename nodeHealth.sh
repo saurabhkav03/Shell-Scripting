a4892a7901e88408ba52376c2d80238ad5f71f0c
saurabhkav03 <kaverisaurabh1998@gmail.com>
2024-03-10 14:40:48 +0530

    Create nodeHealth.sh
    This is the bash file to check the node health

---
+++ nodeHealth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#####################################
+# Author: Saurabh
+# Date: 10/03/2024
+#
+# This script outputs the node health
+#
+# Version: v1
+######################################
+
+set -x # debug mode 
+set -e # exit script when there is an error
+set -o # pipefail
+
+#set -exo # can use all 3 parameters but not recommended
+
+df -h
+
+free -g
+
+nproc
+
+ps -ef | grep python | awk -F" " '{print$2}'  # will print process id
